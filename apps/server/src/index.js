a0d0937d8b2f477f5c210bf8070be37dce9c2fd7
Vishal45718 <vv140220006@gmail.com>
2026-03-17 16:40:21 +0530

    correct initialization order and route setup
    - ensure GridFS initializes after MongoDB connection
    - mount upload routes correctly
    - added middleware (cors,json parsing)
    - uses environment-based port configuration

--- apps/server/src/index.js
+++ apps/server/src/index.js
@@ -2,21 +2,35 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+
 const connectDB = require("./core/db");
 const { initStorage } = require("./core/storage");
 const uploadRoutes = require("./api/upload/upload.routes");
+
 const app = express();
+
 connectDB();
+
+//start GridFS AFTER DB connects
 mongoose.connection.once("open", () => {
   initStorage();
 });
+
+//middleware
 app.use(cors());
 app.use(express.json());
+
+//Routes
 app.use("/api/upload", uploadRoutes);
+
+//check server status
 app.get("/", (req, res) => {
   res.send("Analytics BI Server is Running!!");
 });
+
+//Start the server
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
