ce7dc4a4bfc77ef622c6f0b6698cf6233b9bd700
vhemanth06 <vhemanthreddy06@gmail.com>
2026-03-22 18:34:49 +0530

    removed a file

--- apps/client/src/modules/data-review/index.js
+++ apps/client/src/modules/data-review/index.js
@@ -6,7 +6,7 @@ import './styles/data-review.css';
 // Page components
 export { default as DataReviewPage } from './DataReviewPage';
 export { default as DataReviewModal } from './DataReviewModal';
-export { default as DataReviewDemo } from './DataReviewDemo';
+// export { default as DataReviewDemo } from './DataReviewDemo';
 
 // Individual components
 export { default as DataGrid } from './components/DataGrid';
